92d198a09f155416fc8256b800ac9fbfc639eea6
sergey-goryushko <goryushko.sergiy@pdffiller.team>
2023-04-19 17:48:52 +0200

    I created the same changes for the main branch to create a conflict.

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    //I've added third commit after the second one was reset.
 }
